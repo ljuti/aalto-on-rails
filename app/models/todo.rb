67e13b15d291aab6b91606c1308ca2771cb54a7a
Lauri Jutila <lauri@kiskolabs.com>
2012-04-16 14:15:35 +0300

    Adds validations to Todo so that unit specs pass.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,10 @@
 class Todo < ActiveRecord::Base
   attr_accessible :completed, :title
+
+  # Old style
+  validates_presence_of :title
+  validates_length_of :title, minimum: 5
+
+  # Rails 3 style
+  validates :title, presence: true, length: { minimum: 5 }
 end
