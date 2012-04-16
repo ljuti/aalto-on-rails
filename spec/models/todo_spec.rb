0c8da9b9b9bae0ee3c8f927b3c78d8cf73c20f12
Lauri Jutila <lauri@kiskolabs.com>
2012-04-16 14:14:08 +0300

    Adds simple validation specs for Todo.

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe Todo do
+  describe "#title" do
+    it { should validate_presence_of :title }
+    it { should ensure_length_of(:title).is_at_least(5) }
+  end
 end
